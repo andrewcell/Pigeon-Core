7d3f56ee6cd82ba82ed27eaa4386afd7f7add598
andrewcell <andrew@iryu.kr>
2016-06-23 23:12:12 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PigeonLib;
+
+namespace PigeonCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wl("Starting Pigeon Core...");
+            wl("-----------------------------------");
+            wl("Configurations");
+            wl("-----------------------------------");
+            Class1 cls = new Class1();
+            wl(cls.getConfigurationDesc());
+            Console.ReadLine();
+            wl("Checking Nginx Configuration File..");
+
+            
+        }
+        private static void wl(string data)
+        {
+            Console.WriteLine(data);
+                
+        }
+           
+    }
+}
